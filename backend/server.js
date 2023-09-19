1a9c7e348a554d07175e11f858f23ce3f11a8bea
Dani Torres Torres <dtt1992@gmail.com>
2023-09-19 18:29:23 +0200

    crear el servidor

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,34 @@
 // Importamos las variables de entorno de nuestro fichero .env
 import 'dotenv/config'
+
+// Importamos la dependencia express para crear el servidor
+import express from 'express'
+
+// Importamos la dependencia cors para controlar las peticiones al backend
+import cors from 'cors'
+
+// Importamos la dependencia morgan para que notificar por consola mas detallado
+import morgan from 'morgan'
+
+// Creamos la variable app para crear el servidor
+const app = express()
+
+// Usamos app para que el servidor utiliza morgan
+app.use(morgan('dev'))
+
+// Usamos app para que nuestro servidor entienda los json
+app.use(express.json())
+
+// Usamos app para que nuestro servidor utilice cors
+app.use(cors())
+
+app.use('/', (req, res) => {
+  res.send({
+    status: 'ok'
+  })
+})
+
+// Usamos app.listen para que el servidor se inicie en el puerto que viene desde nuestro .env
+app.listen(process.env.PORT, () => {
+  console.log(`Server iniciado en http://localhost:${process.env.PORT}`)
+})
