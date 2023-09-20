f94fef573c0365dc356758990956dd69137e1edc
Juansito19 <juanbro68@gmail.com>
2023-09-20 14:46:44 +0200

    ajuste de comentarios

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,7 @@
+// Importamos express y Router para crear la ruta.
 import { Router } from 'express'
+
+// Importamos las rutas.
 import userRoutes from './userRoutes.js'
 export const router = Router()
 
