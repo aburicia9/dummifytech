85c219c9ce7c914bece318c9e237efa7318c539f
Juansito19 <juanbro68@gmail.com>
2023-09-21 15:28:46 +0200

    model del avatar

--- backend/src/models/users/updateAvatarModel.js
+++ backend/src/models/users/updateAvatarModel.js
@@ -1,1 +1,21 @@
-import { getDb } from "../../db/getDb.js";
+// Importamos la conexion a la base de datos.
+import { getDb } from '../../db/getDb.js'
+
+// Funcion que se conecta y actualiza la base de datos.
+export const updateAvatarModel = async (avatarName, userId) => {
+  let connection
+
+  try {
+    connection = await getDb()
+
+    await connection.query(`
+    UPDATE users 
+    SET avatar = ?
+    WHERE id = ?`, [
+      avatarName,
+      userId.id
+    ])
+  } finally {
+    if (connection) connection.release()
+  }
+}
