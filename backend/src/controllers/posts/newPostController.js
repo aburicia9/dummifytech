f95116e102a729df8875d7a4793a2c8555fe2792
Carlos Rico <114946697+aburicia9@users.noreply.github.com>
2023-09-22 19:34:25 +0200

    añadimos el post controller

--- backend/src/controllers/posts/newPostController.js
+++ backend/src/controllers/posts/newPostController.js
@@ -1,12 +1,20 @@
 // Importamos el modelo.
+import { fromZodError } from 'zod-validation-error'
 import { insertPostModel } from '../../models/posts/insertPostModel.js'
+import { newPostSchema } from '../../schemas/posts/newPostSchema.js'
 import { savePhoto } from '../../utils/savePhoto.js'
+import { validateSchema } from '../../utils/validateSchema.js'
 
 // Funcion controladora donde se inserta el post.
 export const newPostController = async (req, res, next) => {
   try {
     const { title, post, categoryId } = req.body
 
+    const result = await validateSchema(newPostSchema, { ...req.body, ...req.files, ...req.user.id })
+    if (!result.success) {
+      throw fromZodError(result.error)
+    }
+    // Variable que almacenará el nombre de la imagen(si hay)
     let imgName
 
     // Si existe alguna imagen la guardamos y obtenemos su nombre (hace falta el joi)
