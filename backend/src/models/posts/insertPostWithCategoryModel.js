15bdc57ff7139fcd815233c330339ca8fd6f9d3b
guadix00 <guadit64@gmail.com>
2023-09-25 14:31:54 +0200

    ayuda

--- backend/src/models/posts/insertPostWithCategoryModel.js
+++ backend/src/models/posts/insertPostWithCategoryModel.js
@@ -1,15 +1,26 @@
-import { getDb } from "../../db/getDb.js"
+import { getDb } from '../../db/getDb.js'
 
-
-export const insertPostWithCategoryModel = async () => {
+export const insertPostWithCategoryModel = async (title, post, imgName, categoryId, userId) => {
   let connection
   try {
     connection = await getDb()
+    const [publicationCat] = await connection.query(
+      `SELECT id
+      FROM categories
+      WHERE id_category = ? `
 
-    const [publication] =await connection.query (
-      ``
     )
+    for (const publication of publicationCat) {
+      const [publication] = await connection.query(
+          `INSERT INTO posts (title, post, image, id_category AS categoryId , id_user AS userId)
+          VALUES (?, ?, ?, ?, ?)
+          WHERE id_category = ${publicationCat}`,
+          [title, post, imgName, categoryId, userId]
+
+      )
+      publicationCat.publication = publication
+    }
   } catch (error) {
-    
+
   }
 }
