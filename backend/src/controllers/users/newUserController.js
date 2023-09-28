21d160ca4efe09d7658261d9ac1ef3775c6ae459
Dani Torres Torres <dtt1992@gmail.com>
2023-09-28 15:01:42 +0200

    verificacion de token expirado

--- backend/src/controllers/users/newUserController.js
+++ backend/src/controllers/users/newUserController.js
@@ -1,14 +1,13 @@
 // Importamos los modelos.
 import { fromZodError } from 'zod-validation-error'
 import { insertUserModel } from '../../models/users/insertUserModel.js'
+import { randomUUID } from 'crypto'
 
 // Importamos la funcion de validar esquemas
 import { newUserSchema } from '../../schemas/users/newUserSchema.js'
 // Importamos la funcion que va a validar los esquemas
 import { validateSchema } from '../../schemas/validateSchema.js'
-import { randomUUID } from 'crypto'
-import jwt from 'jsonwebtoken'
-import { getTemplate, sendEmail } from '../../utils/email/configEmail.js'
+import { sendVerificationEmail } from '../../utils/email/sendVerificationEmail.js'
 
 // Funcion controladora final que inserta un nuevo usuario.
 export const newUserController = async (req, res, next) => {
@@ -22,22 +21,10 @@ export const newUserController = async (req, res, next) => {
     if (!result.success) {
       throw fromZodError(result.error)
     }
-
     // Generamos un codigo
     const code = randomUUID()
 
-    // Creamos un token  para verificar si el usuario ha verificado el correo
-    const tokenInfo = {
-      email,
-      code
-    }
-    // Generamos el token temporal para la verificacion
-    const token = jwt.sign(tokenInfo, process.env.SECRET, { expiresIn: '10s' })
-    // Obtenemos el template
-    const template = getTemplate(username, token)
-
-    // Enviamos el correo
-    await sendEmail(email, 'Correo de verificacion de usuario', template)
+    await sendVerificationEmail(email, username, code)
 
     await insertUserModel(username, email, password, fullName, code)
 
