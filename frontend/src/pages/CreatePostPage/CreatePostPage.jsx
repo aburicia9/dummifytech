ca5a2e202f0663be10a65fa100b3a3ec15e5aa12
Juansito19 <juanbro68@gmail.com>
2023-10-07 02:31:32 +0200

    cambios

--- frontend/src/pages/CreatePostPage/CreatePostPage.jsx
+++ frontend/src/pages/CreatePostPage/CreatePostPage.jsx
@@ -1,25 +1,87 @@
+import { useNavigate } from 'react-router-dom'
 import { ButtonComponent } from '../../components/Button/ButtonComponent'
 import { Layout } from '../../components/Layout/Layout'
-import { PostHeaderComponent } from '../../components/PostRandom/PostHeader/PostHeaderComponent'
 import './CreatePostPage.css'
+import { useState } from 'react'
+import { createPostService } from '../../services/postService'
 
-const baseApiURL = import.meta.env.VITE_API_URL
+export const CreatePostPage = ({ avatar }) => {
+  const navigate = useNavigate()
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+  const [category, setCategory] = useState()
+  const [loading, setLoading] = useState(false)
 
-export const CreatePostPage = (post) => {
+  const handleOnChangeTitle = (event) => {
+    setTitle(event.target.value)
+  }
+  const handleOnChangeText = (event) => {
+    setText(event.target.value)
+  }
+  const handleOnChangeCategory = (event) => {
+    setCategory(event.target.value)
+  }
+
+  const resetForm = () => {
+    setTitle()
+    setText()
+  }
+
+  const handleOnClickCreatePost = async () => {
+    try {
+      setLoading(true)
+
+      // const formData = new FormData()
+      // formData.append('text', text)
+
+      const body = await createPostService({ title, text, category })
+
+      if (body.status === 'ok') {
+        resetForm()
+        navigate('/')
+      }
+
+      // if (body.status === 'error') {
+      //   throw new Error(body.message)
+      // }
+    } catch (error) {
+      console.log(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Layout>
       <div className='div-create-post'>
         <form className='form-create-post'>
-          <PostHeaderComponent avatar={post.avatar} username={post.username} baseApiURL={baseApiURL} />
           <h2 className='title-create-post'>¿En que piensas?</h2>
           <label htmlFor='title' className='label-post-create'>Titulo del post:</label>
-          <input type='text' id='title' className='input-post-create' />
+          <input
+            type='text'
+            id='title'
+            className='input-post-create'
+            onChange={handleOnChangeTitle}
+            value={title}
+          />
           <label htmlFor='text' className='label-post-create'>Publicacion:</label>
-          <textarea id='text' cols='30' rows='10' className='input-post-create-txt' />
+          <textarea
+            id='text'
+            cols='30'
+            rows='10'
+            className='input-post-create-txt'
+            onChange={handleOnChangeText}
+            value={text}
+          />
           <div className='button-position'>
             <form class='category-menu'>
-              <label className='button-generic' for='categorias'>Categorias</label>
-              <select id='categorias' class='category-menu-select'>
+              <label className='button-generic' id='categorias'>Categorias</label>
+              <select
+                id='categorias'
+                class='category-menu-select'
+                onChange={handleOnChangeCategory}
+                value={category}
+              >
                 <option className='option-subcategory' value='Hardware'>Hardware</option>
                 <option className='option-subcategory' value='Software'>Software</option>
                 <option className='option-subcategory' value='IA'>IA</option>
@@ -37,7 +99,11 @@ export const CreatePostPage = (post) => {
               </select>
             </form>
             <div className='button-create-post'>
-              <ButtonComponent className='button-generic large' buttonName='Crear Post' />
+              <ButtonComponent
+                className='button-generic large'
+                buttonName='Crear Post'
+                handleOnClick={handleOnClickCreatePost}
+              />
             </div>
           </div>
         </form>
