30969f690a1eed684b244f483a105b258d0bb2e2
guadix00 <guadit64@gmail.com>
2023-10-12 13:18:33 +0200

    control de usuario a medias

--- frontend/src/pages/UsersRolePage/UsersRolePage.jsx
+++ frontend/src/pages/UsersRolePage/UsersRolePage.jsx
@@ -1,10 +1,66 @@
+import { useEffect, useState } from 'react'
 import { Layout } from '../../components/Layout/Layout'
+import { useAuth } from '../../hooks/useAuth'
 import './UsersRolePage.css'
+import { listAllUsersService } from '../../services/authService'
+const baseApiURL = import.meta.env.VITE_API_URL
 
 export const UsersRolePage = () => {
+  const { authUser } = useAuth()
+  const [ users, setUsers ] = useState([])
+  const { isAuthenticated } = useAuth()
+  const [ loading, setLoading ] = useState(false)
+
+  const fetchUsers = async () => {
+    try { 
+      setLoading(true) 
+      let body = []
+      if(isAuthenticated){
+        body = await listAllUsersService()
+
+      }
+      setUsers(body.data.users)
+
+      return setUsers()
+
+    } catch (error) {
+      console.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+
+  }
+  
+
+  useEffect( () => {
+    fetchUsers()
+  }, [])
   return (
     <Layout>
-      <h1>holi</h1>
+      <div className='div-users-change'>
+        <article className='article-users'>
+          <ul className="ul-users">
+            {users.map((user) => {
+              <UserInfo
+              key={users.id}
+              />
+            })}
+          </ul>
+        </article>
+      </div>
     </Layout>
+  )
+}
+
+function UserInfo ({ user, authUsers, setLoading, fetchUsers }) {
+  return(
+    <li key={user.id} className='li-users'>
+      <aside className='aside-header-user'>
+        <img className='img-users-list' src={`${baseApiURL}/avatar/${users.avatar}`} alt="avatar del usuario" />
+
+      </aside>
+
+
+    </li>
   )
 }
